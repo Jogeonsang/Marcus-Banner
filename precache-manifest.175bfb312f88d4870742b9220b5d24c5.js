dffb43d58f6df70ad7966ef3beabfbda2ec51b72
Jogeonsang <rhcn258@gmail.com>
2019-03-18 11:01:21 +0900

    Updates

---
+++ precache-manifest.175bfb312f88d4870742b9220b5d24c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fc0f0d119abb60fdb9b",
+    "url": "/Marcus-Banner/static/js/runtime~main.4096e8ba.js"
+  },
+  {
+    "revision": "7c7c5a3541eaf107e248",
+    "url": "/Marcus-Banner/static/js/main.c409992d.chunk.js"
+  },
+  {
+    "revision": "dcc1a5382b0cf1bfd0e2",
+    "url": "/Marcus-Banner/static/js/2.3021bf8b.chunk.js"
+  },
+  {
+    "revision": "7c7c5a3541eaf107e248",
+    "url": "/Marcus-Banner/static/css/main.c3a6ac7d.chunk.css"
+  },
+  {
+    "revision": "0f97f60aedfa6a3c353758196598b40a",
+    "url": "/Marcus-Banner/index.html"
+  }
+];
